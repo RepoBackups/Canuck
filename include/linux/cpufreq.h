c4ef384f723d33eea6b5f5784ce80e0cdcef9a8f
Dave Savage <bedalus@gmail.com>
2015-02-03 18:28:26 +0000

    Preservative Governor
    Signed-off-by: Dave Savage <bedalus@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -23,18 +23,16 @@
 
 #define CPUFREQ_NAME_LEN 16
 
-<<<<<<< HEAD
 #ifdef CONFIG_CMDLINE_OPTIONS
 /* cmdline_khz variables */
 extern uint32_t cmdline_maxkhz;
 extern char cmdline_gov[16];
 extern int cmdline_gov_cnt;
+#endif
 
-=======
 #ifdef CONFIG_CPU_FREQ_GOV_BADASS_GPU_CONTROL
 /* Badass gpu state detection */
 extern bool gpu_busy_state;
->>>>>>> 41bb571... Badass
 #endif
 
 /*********************************************************************
@@ -409,6 +407,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
 extern struct cpufreq_governor cpufreq_gov_badass;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_badass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PRESERVATIVE)
+extern struct cpufreq_governor cpufreq_gov_preservative;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_preservative)
 #endif
 
 
